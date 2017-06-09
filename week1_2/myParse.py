6f457d74ee4fb1aeca37b975474234c9a5ab9d1d
zzhang115 <zzhang115@dons.usfca.edu>
2017-06-08 23:21:27 -0700

    good job

--- week1_2/myParse.py
+++ week1_2/myParse.py
@@ -3,14 +3,40 @@ import os
 path = './web/new_index.html'
 
 with open(path, 'r') as f:
-    Soup = BeautifulSoup(f.read(), 'lxml')
-    titles = Soup.select('body > div.main-content > ul > li:nth-of-type(1) > div.article-info > h3 > a')
+    Soup = BeautifulSoup(f, 'lxml')
+    # title = Soup.select('body > div.main-content > ul > li:nth-of-type(1) > div.article-info > h3 > a')
+    # image = Soup.select('body > div.main-content > ul > li:nth-of-type(1) > img')
+    # rate= Soup.select('body > div.main-content > ul > li:nth-of-type(1) > div.rate > span')
+    # description = Soup.select('body > div.main-content > ul > li:nth-of-type(1) > div.article-info > p.description')
+    # print (titles)
+    # print (image)
+    # print (title)
+    # print (description)
 
-    print (titles)
+    #get rid of its specific position and get all title like things
+    # allTitle = Soup.select('body > div.main-content > ul > li > div.article-info > h3 > a')
+    # allImage = Soup.select('body > div.main-content > ul > li:nth-of-type(1) > img')
+    # allRate= Soup.select('body > div.main-content > ul > li:nth-of-type(1) > div.rate > span')
+    # allDescription = Soup.select('body > div.main-content > ul > li:nth-of-type(1) > div.article-info > p.description')
+    # allMetadata = Soup.select('body > div.main-content > ul > li:nth-of-type(1) > div.article-info > p.meta-info > span')
+    allTitle = Soup.select('body > div.main-content > ul > li > div.article-info > h3 > a')
+    allImage = Soup.select('body > div.main-content > ul > li > img')
+    allRate= Soup.select('body > div.main-content > ul > li > div.rate > span')
+    allDescription = Soup.select('body > div.main-content > ul > li > div.article-info > p.description')
+    allMetadata = Soup.select('body > div.main-content > ul > li > div.article-info > p.meta-info > span')
+
+    print ("allTitle:", allTitle)
+    print("meta:", allMetadata)
+
+    for title, image, rate, description, metadata in zip(allTitle, allImage, allRate, allDescription, allMetadata):
+        print(title.get_text())
+        print(image.get('src'))
+        print(rate.get_text())
+        print(description.get_text())
+        print(metadata.get_text())
+
 
 '''
-body > div.main-content > ul > li:nth-child(1) > img
-body > div.main-content > ul > li:nth-child(1) > div.article-info > h3 > a
-body > div.main-content > ul > li:nth-child(1) > div.rate > span
-body > div.main-content > ul > li:nth-child(1) > div.article-info > p.description
+
+
 '''
