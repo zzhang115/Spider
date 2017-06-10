786c34fbcde4aa43691f082558d69b2bfdc4ae72
zzhang115 <zzhang115@dons.usfca.edu>
2017-06-09 21:20:48 -0700

    add full code of parse first html

--- week1_2/myParse.py
+++ week1_2/myParse.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import os
 path = './web/new_index.html'
-
+infoList = []
 with open(path, 'r') as f:
     Soup = BeautifulSoup(f, 'lxml')
     # title = Soup.select('body > div.main-content > ul > li:nth-of-type(1) > div.article-info > h3 > a')
@@ -23,20 +23,26 @@ with open(path, 'r') as f:
     allImage = Soup.select('body > div.main-content > ul > li > img')
     allRate= Soup.select('body > div.main-content > ul > li > div.rate > span')
     allDescription = Soup.select('body > div.main-content > ul > li > div.article-info > p.description')
-    allMetadata = Soup.select('body > div.main-content > ul > li > div.article-info > p.meta-info > span')
+    allMetadata = Soup.select('body > div.main-content > ul > li > div.article-info > p.meta-info')
 
-    print ("allTitle:", allTitle)
-    print("meta:", allMetadata)
+    # print("allTitle:", allTitle)
+    # print("meta:", allMetadata)
+for title, image, rate, description, metadata in zip(allTitle, allImage, allRate, allDescription, allMetadata):
+        info = {
+            "title" : title.get_text(),
+            "image" : image.get('src'), #src is one attribute in image selector
+            "rate" : rate.get_text(),
+            "description" : description.get_text(),
+            "cate" : list(metadata.stripped_strings),
+        }
+        infoList.append(info)
+for information in infoList:
+   if float(information['rate']) >= 4:
+       print(information['title'])
 
-    for title, image, rate, description, metadata in zip(allTitle, allImage, allRate, allDescription, allMetadata):
-        print(title.get_text())
-        print(image.get('src'))
-        print(rate.get_text())
-        print(description.get_text())
-        print(metadata.get_text())
+    # print(title.get_text())
+    # print(image.get('src'))
+    # print(rate.get_text())
+    # print(description.get_text())
+    # print(metadata.get_text())
 
-
-'''
-
-
-'''
