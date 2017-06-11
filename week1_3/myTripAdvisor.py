56660021f348fe21da097bc78d097cc2ee86f9b2
zzhang115 <zzhang115@dons.usfca.edu>
2017-06-10 22:02:58 -0700

    finish myTripAdvisor.py

--- week1_3/myTripAdvisor.py
+++ week1_3/myTripAdvisor.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+
+data = []
 url = 'https://cn.tripadvisor.com/Attractions-g60763-Activities-New_York_City_New_York.html'
 webdata = requests.get(url)
 # print(webdata.text)
@@ -12,14 +14,23 @@ titles = soup.select('a.poiTitle')#[target="_blank"]')
 images = soup.select('img[width="200"]')# set image width can eliminate other images we dont need
 cates = soup.select('div.detail > div:nth-of-type(3)')# > div:nth-of-type(1)')# > div:nth-of-type(1) > div:nth-of-type(2) > div')
 # print(titles)
-print(images)
-print(cates)
+# print(images)
+# print(cates)
 
 # for title in titles:
 #     print(title.get_text())
 # for image in images:
 #     print(image.get('src'))
-for cate in cates:
-    print(cate.get_text())
-# titles = soup.select('.top_attractions > div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(2)') > div:nth-of-type(1) > a')
-# .top_attractions > div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type(1) > a:nth-of-type(1)
+# for cate in cates:
+#     print(cate.get_text())
+for title, image, cate in zip(titles, images, cates):
+    info = {
+        "title:" : title.get_text(),
+        "image:" : image.get('src'),
+        "cate:" : cate.get_text(),
+    }
+    data.append(info)
+
+for d in data:
+    print(d)
+
