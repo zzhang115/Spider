17e0f6b7a684c3b182d41424a8fdc9841adf8bf0
zzhang115 <zzhang115@dons.usfca.edu>
2017-06-17 22:36:41 -0700

    add query sentence

--- week2_1/mongodb.py
+++ week2_1/mongodb.py
@@ -1,16 +1,23 @@
 import pymongo
 
-client = pymongo.MongoClient('admin', 'admin123')
+client = pymongo.MongoClient('localhost', 27017)
 admin = client['admin'] # table name in mongodb
-sheet_lines = admin['sheel_lines']
+sheet_table= admin['sheel_tab']
 
-path = ''
-with open(path, 'r') as file:
-    lines = file.readlines()
-    for index, line in enumerate(lines):
-        data = {
-           'index' : index,
-            'line' : line,
-            'words' : len(line.split())
-        }
-        print(data)
+# path = 'walden.txt'
+# with open(path, 'r') as file:
+#     lines = file.readlines()
+#     for index, line in enumerate(lines):
+#         data = {
+#            'index' : index,
+#             'line' : line,
+#             'words' : len(line.split())
+#         }
+#         print(data)
+#         sheet_lines.insert_one(data)
+# for item in sheet_table.find({'words':0}): # query dict structure
+#     print(item)
+# for item in sheet_table.find(): # query dict structure
+#     print(item['line'])
+for item in sheet_table.find({'words':{'$lt':5}}): # query dict structure
+    print(item)
