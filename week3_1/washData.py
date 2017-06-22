0ce9750e2da7117362fd403fecc1c8edcaf3eeb4
zzhang115 <zzhang115@dons.usfca.edu>
2017-06-21 23:57:01 -0700

    oh shit 70000 datas destroyed!

--- week3_1/washData.py
+++ week3_1/washData.py
@@ -4,6 +4,12 @@ from string import punctuation
 client = pymongo.MongoClient('localhost', 27017)
 tongcheng = client['tongcheng']
 item_info = tongcheng['item_info']
+# for i in item_info.find():#.limit(300):
+#     if i['price']:
+#         price = i['price']
+#         price = price[1 : len(price)]
+#         print(price)
+        # item_info.update({'_id' : i['_id']}, {'price' : price})
 for i in item_info.find().limit(300):
     print(i)
 
@@ -14,3 +20,7 @@ for i in item_info.find().limit(300):
 #     else:
 #         area = 'Undefined'
 #     print(area)
+# test = client['test']
+# testlist = test['testlist']
+# for i in testlist.find():
+#     testlist.update({'_id' : i['_id']}, {'value' : 'b'})
